fe7ba366653f1e5a90c88c503004c5d5ab2a30a2
anthony payne <anthonypayne942@gmail.com>
2018-03-01 20:53:50 -0800

    started firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,18 @@
 $(document).ready(function () {
-    
-    
+
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyDcy3WxCDC1Biy5PTv1ILvMSSlbJM1iVkw",
+        authDomain: "timesheetmf.firebaseapp.com",
+        databaseURL: "https://timesheetmf.firebaseio.com",
+        projectId: "timesheetmf",
+        storageBucket: "",
+        messagingSenderId: "274605900626"
+    };
+    firebase.initializeApp(config);
+
     console.log('ready')
-    
+
     // Data Picker Initialization
     $(function () {
         $('#datetimepicker1').datetimepicker();
@@ -12,3 +22,4 @@ $(document).ready(function () {
     $('.datepicker').pickadate();
 
 })
+
