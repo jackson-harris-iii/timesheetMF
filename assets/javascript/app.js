3a6cd62502e13c8735cab8715c5a039d55e1885d
Apollo <jackson.harris3@gmail.com>
2018-03-01 19:32:56 -0800

    create base styling

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    
+    
+    console.log('ready')
+    
+    // Data Picker Initialization
+    $(function () {
+        $('#datetimepicker1').datetimepicker();
+    });
+
+    // Data Picker Initialization
+    $('.datepicker').pickadate();
+
+})
